91fda72a4a2f11be1179dafdaa7d6c1ae2f3c88b
wnvei <wvei.biz@gmail.com>
2025-04-25 21:36:17 +0530

    Updated Files

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,10 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root123',
-  database: 'Airport_Management',
+  host: '',
+  user: '',
+  password: '',
+  database: '',
 });
 
 app.post('/login', (req, res) => {
